d2f0c4771f024ffd66392a82a4d99e85e97c8b8d
Alexey Korablin <alex8456@bk.ru>
2020-04-27 17:22:32 +0300

    #09 Pre-Caching assets

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/materialize.min.js',
+  '/js/ui.js',
+  '/css/materialize.min.css',
+  '/css/styles.css',
+  '/favicon.ico',
+  '/img/dish.jpg ',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 self.addEventListener('install', (evt) => {
   console.log('SW has been installed!', evt);
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('cashing shell assets');
+      cache.addAll(assets);
+    }),
+  );
 });
 
 self.addEventListener('activate', (evt) => {
