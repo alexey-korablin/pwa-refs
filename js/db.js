43d70103ca6a0f5c5e63a360548a9a3f83c23637
Alexey Korablin <alex8456@bk.ru>
2020-04-28 17:15:19 +0300

    #16 Realtime data

--- js/db.js
+++ js/db.js
@@ -1,1 +1,13 @@
-console.log('db works');
+db.collection('recipes').onSnapshot((snapshot) => {
+  console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach((change) => {
+    console.log(change.doc.id);
+    console.log(change.doc.data());
+    if (change.type === 'added') {
+      // TODO add the document data to the web page
+    }
+    if (change.type === 'removed') {
+      // TODO remove the document data from the web page
+    }
+  });
+});
