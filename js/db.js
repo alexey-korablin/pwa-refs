70d895e7af4f75d4f8ce06fa2220875ed4f3cae1
Alexey Korablin <alex8456@bk.ru>
2020-04-29 17:26:32 +0300

    #17 Updates the UI on getting data from db [offline support is currently off]

--- js/db.js
+++ js/db.js
@@ -4,7 +4,7 @@ db.collection('recipes').onSnapshot((snapshot) => {
     console.log(change.doc.id);
     console.log(change.doc.data());
     if (change.type === 'added') {
-      // TODO add the document data to the web page
+      renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === 'removed') {
       // TODO remove the document data from the web page
